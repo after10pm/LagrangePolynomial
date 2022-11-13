9f15492930a6ce02f4aaed38584863858a93ff07
Gol-dey <golovlev.gordey1@gmail.com>
2022-11-13 11:14:50 +0300

    a bit later we will be able to launch the main window!!!

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MainWindow window = new MainWindow();
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        window.setSize(8000, 6000);
+        window.setVisible(true);
+    }
+}
